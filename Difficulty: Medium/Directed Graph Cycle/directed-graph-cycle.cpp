b0e632edb25e7348640e407e09b0fd111dc0f1c5
Pratham-Sharda <144925900+Pratham-Sharda@users.noreply.github.com>
2024-07-07 12:46:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -5,36 +5,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    bool dfs_cycle(int node,vector<int>& visited,vector<int>& pathvisited,vector<int> adj[]){
+    bool dfs(int node,vector<int>& visited,vector<int>& pathvis,vector<int> adj[]){
         visited[node]=1;
-        pathvisited[node]=1;
+        pathvis[node]=1;
         
-        for(auto i:adj[node]){
-            if(!visited[i]){
-                if(dfs_cycle(i,visited,pathvisited,adj)){
+        for(auto it:adj[node]){
+            if(!visited[it]){
+                if(dfs(it,visited,pathvis,adj)==true){
                     return true;
                 }
-            }else if(pathvisited[i]){
+            }else if(pathvis[it]){
                 return true;
             }
         }
         
-        pathvisited[node]=0;
+        pathvis[node]=0;
         return false;
     }
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
         vector<int> visited(V,0);
-        vector<int> pathvisited(V,0);
+        vector<int> pathvis(V,0);
         
         for(int i=0;i<V;i++){
             if(!visited[i]){
-                if(dfs_cycle(i,visited,pathvisited,adj)){
-                    return true;
+                if(dfs(i,visited,pathvis,adj)==true){
+                 return true;   
                 }
             }
         }
+        
         return false;
     }
 };
